5bd6b474c006d2cde2ea452e54cee8d9ec7f3661
hamdanisaja007-star <hamdanisaja007@gmail.com>
2026-02-09 16:20:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template, request, jsonify
-import os
 
-# Mencari alamat folder tempat app.py berada secara otomatis
-base_dir = os.path.dirname(os.path.abspath(__file__))
-
-app = Flask(__name__, template_folder=base_dir)
+# Flask secara otomatis akan mencari folder bernama 'templates'
+app = Flask(__name__)
 
 @app.route('/')
 def login_page():
-    # Flask akan mencari login.html di folder utama (base_dir)
+    # Flask akan otomatis mencari templates/login.html
     return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard_page():
-    # Flask akan mencari index.html di folder utama (base_dir)
+    # Flask akan otomatis mencari templates/index.html
     return render_template('index.html')
 
 @app.route('/handler', methods=['POST'])
