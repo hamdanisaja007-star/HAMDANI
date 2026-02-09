e67b1d6a4660e2ec25d737fd2c6090019283b3ba
hamdanisaja007-star <hamdanisaja007@gmail.com>
2026-02-09 16:07:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, request, jsonify
 import os
 
-# Paksa Flask mencari file HTML di folder yang sama dengan app.py
-app = Flask(__name__, template_folder='.')
+# Mencari alamat folder tempat app.py berada secara otomatis
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__, template_folder=base_dir)
 
 @app.route('/')
 def login_page():
-    # Pastikan file Bapak namanya persis: login.html
+    # Flask akan mencari login.html di folder utama (base_dir)
     return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard_page():
-    # Pastikan file Bapak namanya persis: index.html
+    # Flask akan mencari index.html di folder utama (base_dir)
     return render_template('index.html')
 
 @app.route('/handler', methods=['POST'])
 def handler():
     return jsonify({"status": "success"})
 
-# Ini penting untuk Vercel agar tidak crash
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
