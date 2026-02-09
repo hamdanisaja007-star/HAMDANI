69531d6f56e295f6ad2707080bdfe09feb0e5e13
hamdanisaja007-star <hamdanisaja007@gmail.com>
2026-02-09 16:29:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
+# Flask akan otomatis mencari folder 'templates' yang sudah Bapak buat
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1>MESIN SIKEPAL SUDAH NYALA!</h1><p>Kalau tulisan ini muncul, berarti Python sudah aman.</p>"
+def login_page():
+    # Ini akan membuka file login.html di folder templates
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard_page():
+    # Ini akan membuka file index.html di folder templates
+    return render_template('index.html')
+
+@app.route('/handler', methods=['POST'])
+def handler():
+    return jsonify({"status": "success"})
 
 if __name__ == '__main__':
     app.run()
