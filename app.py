a73aae7658f7c07274d8f853c12b690f0f96ff96
hamdanisaja007-star <hamdanisaja007@gmail.com>
2026-03-29 14:26:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,15 @@ class LogBaca(db.Model):
     nama_pembaca = db.Column(db.String(100))
     waktu_baca = db.Column(db.DateTime, default=datetime.now)
 
+# MODEL BARU: Untuk Chat Dua Arah
+class PesanChat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer) # ID Pengirim
+    nama_pengirim = db.Column(db.String(100))
+    role = db.Column(db.String(10)) # 'admin' atau 'user'
+    isi_pesan = db.Column(db.Text, nullable=False)
+    waktu = db.Column(db.DateTime, default=datetime.now)
+
 # --- MIDDLEWARE ---
 @app.context_processor
 def inject_user():
@@ -76,6 +85,7 @@ def auth():
         session.clear()
         session['user_role'] = 'admin'
         session['user_nip'] = 'ADMIN'
+        session['user_id'] = 0 # ID khusus admin
         return redirect(url_for('dashboard'))
     
     user = Pegawai.query.filter_by(nip=u).first()
@@ -86,7 +96,7 @@ def auth():
         session['user_id'] = user.id
         return redirect(url_for('dashboard'))
     
-    flash("NIP atau Password salah! (Gunakan NIP sebagai password)", "danger")
+    flash("NIP atau Password salah!", "danger")
     return redirect(url_for('landing'))
 
 @app.route('/dashboard')
@@ -109,6 +119,64 @@ def dashboard():
     return render_template('index.html', pegawai=pegawai_list, role=role, 
                            notif_pangkat=notif_pangkat, notif_pensiun=notif_pensiun)
 
+# --- FITUR CHAT & INFO (DIPERBARUI) ---
+@app.route('/chat_info')
+def chat_info():
+    if 'user_role' not in session: return redirect(url_for('landing'))
+    role = session['user_role']
+    
+    # Ambil Pengumuman Resmi
+    info = Pengumuman.query.order_by(Pengumuman.tanggal.desc()).all()
+    
+    # Ambil Percakapan Chat (Batas 50 pesan terakhir)
+    all_messages = PesanChat.query.order_by(PesanChat.waktu.asc()).all()
+
+    # Logika Log Baca untuk Admin
+    if role != 'admin':
+        user = Pegawai.query.filter_by(nip=session['user_nip']).first()
+        for i in info:
+            if not LogBaca.query.filter_by(pengumuman_id=i.id, nip_pembaca=user.nip).first():
+                db.session.add(LogBaca(pengumuman_id=i.id, nip_pembaca=user.nip, nama_pembaca=user.nama))
+        db.session.commit()
+    
+    logs = {i.id: LogBaca.query.filter_by(pengumuman_id=i.id).all() for i in info} if role == 'admin' else {}
+    
+    return render_template('chat_info.html', info_terkini=info, logs=logs, role=role, all_messages=all_messages)
+
+@app.route('/kirim_pesan', methods=['POST'])
+def kirim_pesan():
+    if 'user_role' not in session: return redirect(url_for('landing'))
+    
+    isi = request.form.get('pesan')
+    role = session.get('user_role')
+    user_id = session.get('user_id')
+    
+    # Cari nama pengirim
+    if role == 'admin':
+        nama = "ADMIN SIDALAP"
+    else:
+        user = Pegawai.query.get(user_id)
+        nama = user.nama if user else "PLKB"
+
+    if isi:
+        baru = PesanChat(user_id=user_id, nama_pengirim=nama, role=role, isi_pesan=isi)
+        db.session.add(baru)
+        db.session.commit()
+    
+    return redirect(url_for('chat_info'))
+
+@app.route('/kirim_info', methods=['POST'])
+def kirim_info():
+    if session.get('user_role') == 'admin':
+        pesan, file = request.form.get('isi'), request.files.get('lampiran')
+        fname = secure_filename(file.filename) if file and file.filename != '' else None
+        if fname: file.save(os.path.join(app.config['CHAT_UPLOAD'], fname))
+        db.session.add(Pengumuman(isi=pesan, file_lampiran=fname))
+        db.session.commit()
+        flash("Pengumuman berhasil disebarkan!", "success")
+    return redirect(url_for('chat_info'))
+
+# --- FUNGSI LAINNYA (PROFIL, BERKAS, DLL TETAP SAMA) ---
 @app.route('/data_pegawai', methods=['GET', 'POST'])
 def data_pegawai():
     if session.get('user_role') != 'admin': return redirect(url_for('landing'))
@@ -123,11 +191,9 @@ def data_pegawai():
             flash(f"PLKB {nama} berhasil didaftarkan!", "success")
         else:
             flash(f"NIP {nip} sudah ada!", "warning")
-    
     pegawai = Pegawai.query.all()
     return render_template('data_pegawai.html', pegawai=pegawai)
 
-# --- FITUR PROFIL ---
 @app.route('/profil')
 def profil():
     if 'user_nip' not in session: return redirect(url_for('landing'))
@@ -140,8 +206,10 @@ def update_profil():
     user = Pegawai.query.filter_by(nip=session.get('user_nip')).first()
     if user:
         try:
-            user.jabatan, user.pangkat_gol = request.form.get('jabatan'), request.form.get('pangkat_gol')
-            user.kecamatan, user.desa_binaan = request.form.get('kecamatan'), request.form.get('desa_binaan')
+            user.jabatan = request.form.get('jabatan')
+            user.pangkat_gol = request.form.get('pangkat_gol')
+            user.kecamatan = request.form.get('kecamatan')
+            user.desa_binaan = request.form.get('desa_binaan')
             user.no_hp = request.form.get('no_hp')
             tmt, tgl = request.form.get('tmt_pangkat'), request.form.get('tgl_lahir')
             if tmt: user.tmt_pangkat = datetime.strptime(tmt, '%Y-%m-%d').date()
@@ -164,7 +232,6 @@ def update_profil():
 def serve_foto(filename):
     return send_from_directory(app.config['PROFIL_UPLOAD'], filename)
 
-# --- FITUR BERKAS ---
 @app.route('/upload_berkas')
 def berkas_saya():
     if 'user_nip' not in session: return redirect(url_for('landing'))
@@ -204,31 +271,6 @@ def berkas_masuk():
 @app.route('/buka_berkas/<nip>/<filename>')
 def download_file(nip, filename):
     return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], nip), filename)
-
-# --- KOMUNIKASI ---
-@app.route('/chat_info')
-def chat_info():
-    if 'user_role' not in session: return redirect(url_for('landing'))
-    role, info = session['user_role'], Pengumuman.query.order_by(Pengumuman.tanggal.desc()).all()
-    if role != 'admin':
-        user = Pegawai.query.filter_by(nip=session['user_nip']).first()
-        for i in info:
-            if not LogBaca.query.filter_by(pengumuman_id=i.id, nip_pembaca=user.nip).first():
-                db.session.add(LogBaca(pengumuman_id=i.id, nip_pembaca=user.nip, nama_pembaca=user.nama))
-        db.session.commit()
-    logs = {i.id: LogBaca.query.filter_by(pengumuman_id=i.id).all() for i in info} if role == 'admin' else {}
-    return render_template('chat_info.html', info_terkini=info, logs=logs, role=role)
-
-@app.route('/kirim_info', methods=['POST'])
-def kirim_info():
-    if session.get('user_role') == 'admin':
-        pesan, file = request.form.get('isi'), request.files.get('lampiran')
-        fname = secure_filename(file.filename) if file and file.filename != '' else None
-        if fname: file.save(os.path.join(app.config['CHAT_UPLOAD'], fname))
-        db.session.add(Pengumuman(isi=pesan, file_lampiran=fname))
-        db.session.commit()
-        flash("Pengumuman berhasil disebarkan!", "success")
-    return redirect(url_for('chat_info'))
 
 @app.route('/logout')
 def logout():
